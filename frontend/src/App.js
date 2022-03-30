fdf00581f43337093cafe26d8b7842dd374fa868
atgane <hyper2012@naver.com>
2022-03-30 22:16:41 +0900

    delete main, signin, signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './Pages/Main';
-import Signin from './Pages/Signin';
-import Signup from './Pages/Signup';
 import NotFound from './Pages/NotFound'
 import CreateRoom from './Pages/CreateRoom';
 import Room from './Pages/Room';
@@ -15,12 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Signin />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<OauthMain />} />
         <Route path='/oauth' element={<Oauth />} />
         <Route path='/oauth_main' element={<OauthMain />} />
         <Route path='/set_name' element={<SetName />} />
-        <Route path='/main' element={<Main />}></Route>
         <Route path='/room/create' element={<CreateRoom />} />
         <Route path='/room/join' element={<JoinRoom />} />
         <Route path='/room' element={<Room />} />
